a8a0fa1fbcad83ddaedd3b13bf3d33d1b37e8900
rjx <f862250480@qq.com>
2024-01-31 16:51:23 +0800

    feature: add neoxxx

--- lua/user/lsp/configs.lua
+++ lua/user/lsp/configs.lua
@@ -1,55 +1,52 @@
 local status_ok, lsp_installer = pcall(require, "nvim-lsp-installer")
 
 if not status_ok then
-	return
+        return
 end
 
 local lspconfig = require("lspconfig")
 local servers = { "pyright", "biome", "ruff_lsp", "clangd", "lua_ls" }
 
 lsp_installer.setup({
-	ensure_installed = servers,
+        ensure_installed = servers,
 })
 
 local root_files = {
-	--"pyproject.toml",
-	--"setup.py",
-	--"setup.cfg",
-	--"requirements.txt",
-	--"Pipfile",
-	--"manage.py",
-	"pyrightconfig.json",
-	".git",
-	".gitignore",
+        --"pyproject.toml",
+        --"setup.py",
+        --"setup.cfg",
+        --"requirements.txt",
+        --"Pipfile",
+        --"manage.py",
+        "pyrightconfig.json",
+        ".git",
+        ".gitignore",
 }
 
 for _, server in pairs(servers) do
-	local opts = {
-		--root_dir = require("lspconfig.util").root_pattern(unpack(root_files)),
+        local opts = {
+                --root_dir = require("lspconfig.util").root_pattern(unpack(root_files)),
 
-		on_attach = require("user.lsp.handlers").on_attach,
-		capabilities = require("user.lsp.handlers").capabilities,
-		--settings = {
-		--python = {
-		--analysis = {
-		--autoSearchPaths = true,
-		--diagnosticMode = "workspace",
-		--useLibraryCodeForTypes = false,
-		--typeCheckingMode = "off",
-		--},
-		--},
-		--}
-	}
-	lspconfig[server].setup(opts)
+                on_attach = require("user.lsp.handlers").on_attach,
+                capabilities = require("user.lsp.handlers").capabilities,
+                settings = {
+                        Lua = {
+                                diagnostics = {
+                                        -- Get the language server to recognize the `vim` global
+                                        globals = { 'vim' },
+                                },
+                        },
+                }
+
+                --python = {
+                --analysis = {
+                --autoSearchPaths = true,
+                --diagnosticMode = "workspace",
+                --useLibraryCodeForTypes = false,
+                --typeCheckingMode = "off",
+                --},
+                --},
+                --}
+        }
+        lspconfig[server].setup(opts)
 end
-
-require'lspconfig'.lua_ls.setup {
-  settings = {
-    Lua = {
-      diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = {'vim'},
-      },
-    },
-  },
-}
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -280,5 +280,26 @@ local plugins = {
         },
         -- startuptime
         "dstein64/vim-startuptime",
+        {
+                "nvim-neorg/neorg",
+                build = ":Neorg sync-parsers",
+                -- tag = "*",
+                dependencies = { "nvim-lua/plenary.nvim" },
+                config = function()
+                        require("neorg").setup {
+                                load = {
+                                        ["core.defaults"] = {}, -- Loads default behaviour
+                                        ["core.concealer"] = {}, -- Adds pretty icons to your documents
+                                        ["core.dirman"] = { -- Manages Neorg workspaces
+                                                config = {
+                                                        workspaces = {
+                                                                notes = "~/notes",
+                                                        },
+                                                },
+                                        },
+                                },
+                        }
+                end,
+        },
 }
 return lazy.setup(plugins, opt)
