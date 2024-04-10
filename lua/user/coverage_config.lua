ade95713eff8c56da40f88dbde41c140dab7716a
renjinxi <48796935+renjinxi@users.noreply.github.com>
2024-04-10 15:04:26 +0800

    Update coverage configuration and add auto-load for Python

--- lua/user/coverage_config.lua
+++ lua/user/coverage_config.lua
@@ -1,42 +1,47 @@
-require("coverage").setup({
-	commands = true, -- create commands
-	highlights = {
-		-- customize highlight groups created by the plugin
-		covered = { fg = "#C3E88D" },   -- supports style, fg, bg, sp (see :h highlight-gui)
-		uncovered = { fg = "#F07178" },
-	},
-	signs = {
-		-- use your own highlight groups or text markers
-		covered = { hl = "CoverageCovered", text = "▎" },
-		uncovered = { hl = "CoverageUncovered", text = "▎" },
-	},
-	summary = {
-		-- customize the summary pop-up
-		min_coverage = 80.0,      -- minimum coverage threshold (used for highlighting)
-	},
-	lang = {
-		-- customize language specific settings
-	},
+local coverage = require("coverage")
+
+local show_coverage = false
+
+local function toggle_coverage(ev)
+    show_coverage = not show_coverage
+    coverage.load(show_coverage)
+end
+
+local function load_cb()
+    if show_coverage then
+        coverage.show()
+    else
+        coverage.hide()
+    end
+end
+
+-- Setup coverage
+coverage.setup({
+    auto_reload = true,
+    load_coverage_cb = load_cb,
+    commands = true, -- create commands
+    highlights = {
+        -- customize highlight groups created by the plugin
+        covered = { fg = "#C3E88D" }, -- supports style, fg, bg, sp (see :h highlight-gui)
+        uncovered = { fg = "#F07178" },
+    },
+    signs = {
+        -- use your own highlight groups or text markers
+        covered = { hl = "CoverageCovered", text = "▎" },
+        uncovered = { hl = "CoverageUncovered", text = "▎" },
+    },
+    summary = {
+        -- customize the summary pop-up
+        min_coverage = 80.0, -- minimum coverage threshold (used for highlighting)
+    },
+    lang = {
+        -- customize language specific settings
+    },
 })
---require("coverage").setup({
-    --commands = true, -- create commands
-    --highlights = {
-        ---- customize highlight groups created by the plugin
-        ----covered = { fg = "#A6E22E" }, -- 使用淡橄榄绿表示已覆盖代码
-        --covered = { fg = "#afd700" },
-        ----uncovered = { fg = "#FD971F" }, -- 使用明亮的橙色表示未覆盖代码
-        --uncovered = { fg = "#dfaf8f" },
-    --},
-    --signs = {
-        ---- use your own highlight groups or text markers
-        --covered = { hl = "CoverageCovered", text = "▎" },
-        --uncovered = { hl = "CoverageUncovered", text = "▎" },
-    --},
-    --summary = {
-        ---- customize the summary pop-up
-        --min_coverage = 80.0, -- minimum coverage threshold (used for highlighting)
-    --},
-    --lang = {
-        ---- customize language specific settings
-    --},
---})
+
+-- Auto-load for python
+vim.api.nvim_create_autocmd('FileType', {
+    pattern = 'python',
+    desc = 'Auto-load coverage in python projects',
+    callback = coverage.load,
+})
