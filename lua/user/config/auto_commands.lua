9500ea667da3028799d770c262b7a2128d53951a
renjinxi <48796935+renjinxi@users.noreply.github.com>
2024-07-02 17:41:29 +0800

    Update auto commands and lualine setup

--- lua/user/config/auto_commands.lua
+++ lua/user/config/auto_commands.lua
@@ -4,92 +4,94 @@ local opt = vim.opt
 local indent_group = api.nvim_create_augroup("indent_group", { clear = true })
 local relative_group = api.nvim_create_augroup("relative_group", { clear = true })
 
-vim.api.nvim_create_augroup('NoHlSearch', { clear = true })
-vim.api.nvim_create_autocmd('BufEnter', {
-    group = 'NoHlSearch',
-    callback = function()
-        vim.opt.hlsearch = false
-    end,
+vim.api.nvim_create_augroup("NoHlSearch", { clear = true })
+vim.api.nvim_create_autocmd("BufEnter", {
+	group = "NoHlSearch",
+	callback = function()
+		vim.opt.hlsearch = false
+	end,
 })
 
 local function set_indent()
-    local default_value = 4
-    local indent_table = {
-        python = 4,
-        markdown = 4,
-        --toml = 2,
-        --c = 4,
-    }
+	local default_value = 4
+	local indent_table = {
+		python = 4,
+		markdown = 4,
+		--toml = 2,
+		--c = 4,
+	}
 
-    opt.expandtab = true
-    opt.autoindent = true
-    for key, value in pairs(indent_table) do
-        if vim.bo.filetype == key then
-            opt.tabstop = value
-            opt.softtabstop = value
-            opt.shiftwidth = value
-            return
-        end
-    end
-    opt.tabstop = default_value
-    opt.softtabstop = default_value
-    opt.shiftwidth = default_value
+	if vim.bo.filetype == "make" then
+		opt.expandtab = false
+	else
+		opt.expandtab = true
+	end
+	opt.autoindent = true
+	for key, value in pairs(indent_table) do
+		if vim.bo.filetype == key then
+			opt.tabstop = value
+			opt.softtabstop = value
+			opt.shiftwidth = value
+			return
+		end
+	end
+	opt.tabstop = default_value
+	opt.softtabstop = default_value
+	opt.shiftwidth = default_value
 end
 
 api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
-    pattern = "*",
-    group = indent_group,
-    desc = "set indent for file",
-    callback = set_indent,
+	pattern = "*",
+	group = indent_group,
+	desc = "set indent for file",
+	callback = set_indent,
 })
 
 -- reload buffer file when file changed on disk
 vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
-    command = "if mode() != 'c' | checktime | endif",
-    pattern = { "*" },
+	command = "if mode() != 'c' | checktime | endif",
+	pattern = { "*" },
 })
 
-
 local function set_relativenumber()
-    vim.opt.relativenumber = true
+	vim.opt.relativenumber = true
 end
 
 local function set_norelativenumber()
-    vim.opt.relativenumber = false
+	vim.opt.relativenumber = false
 end
 api.nvim_create_autocmd({ "WinEnter", "BufRead", "BufEnter", "FocusGained" }, {
-    pattern = "*",
-    group = relative_group,
-    desc = "set relativenumber for file",
-    callback = set_relativenumber,
+	pattern = "*",
+	group = relative_group,
+	desc = "set relativenumber for file",
+	callback = set_relativenumber,
 })
 
 api.nvim_create_autocmd({ "WinLeave", "BufLeave", "FocusLost" }, {
-    --api.nvim_create_autocmd({ "FocusLost" }, {
-    pattern = "*",
-    group = relative_group,
-    desc = "set norelativenumber for file",
-    callback = set_norelativenumber,
+	--api.nvim_create_autocmd({ "FocusLost" }, {
+	pattern = "*",
+	group = relative_group,
+	desc = "set norelativenumber for file",
+	callback = set_norelativenumber,
 })
-
 
 local lsp = vim.lsp
 local lsp_group = api.nvim_create_augroup("lsp_group", { clear = true })
 local function stop_all_lsp()
-    lsp.stop_client(lsp.get_active_clients())
+	lsp.stop_client(lsp.get_active_clients())
 end
 
 local function start_lsp()
-    vim.cmd("LspStart")
+	vim.cmd("LspStart")
 end
 
 api.nvim_create_autocmd("TabEnter", {
-    group = "lsp_group", -- 使用前面创建的自动命令组
-    callback = function()
-        -- 在切换到新的 tab 时先停止所有的 LSP
-        stop_all_lsp()
+	group = "lsp_group", -- 使用前面创建的自动命令组
+	callback = function()
+		-- 在切换到新的 tab 时先停止所有的 LSP
+		stop_all_lsp()
 
-        -- 等待一段时间后重新启动 LSP
-        vim.defer_fn(start_lsp, 1000)
-    end,
+		-- 等待一段时间后重新启动 LSP
+		vim.defer_fn(start_lsp, 1000)
+	end,
 })
--- lua/user/plugins/lualine.lua
+++ lua/user/plugins/lualine.lua
@@ -1,101 +1,102 @@
 local window = function()
-    return vim.api.nvim_win_get_number(0)
+	return vim.api.nvim_win_get_number(0)
 end
 
 local cond200 = function()
-    return vim.fn.winwidth(0) > 200
+	return vim.fn.winwidth(0) > 200
 end
 local cond80 = function()
-    return vim.fn.winwidth(0) > 80
+	return vim.fn.winwidth(0) > 80
 end
 local cond40 = function()
-    return vim.fn.winwidth(0) > 40
+	return vim.fn.winwidth(0) > 40
 end
 
 local codeium_state = function()
-    return vim.api.nvim_call_function("codeium#GetStatusString", {})
+	return vim.api.nvim_call_function("codeium#GetStatusString", {})
 end
 
 local function clock()
-    return os.date('%Y-%m-%d %H:%M') -- Formats time in HH:MM:SS format
+	-- return os.date('%Y-%m-%d %H:%M') -- Formats time in HH:MM:SS format
+	return os.date("%H:%M") -- Formats time in HH:MM:SS format
 end
 
 local function get_project_root()
-    local cwd = vim.loop.cwd()
-    while true do
-        if vim.loop.fs_stat(cwd .. '/.git') then
-            return cwd:match("([^/\\]+)$") -- returns the name of the current directory
-        end
-        local parent = cwd:match("(.*)[/\\]")
-        if not parent or parent == cwd then break end
-        cwd = parent
-    end
-    return '[No Project]' -- default name if no .git directory is found
+	local cwd = vim.loop.cwd()
+	while true do
+		if vim.loop.fs_stat(cwd .. "/.git") then
+			return cwd:match("([^/\\]+)$") -- returns the name of the current directory
+		end
+		local parent = cwd:match("(.*)[/\\]")
+		if not parent or parent == cwd then
+			break
+		end
+		cwd = parent
+	end
+	return "[No Project]" -- default name if no .git directory is found
 end
 
 require("lualine").setup({
-    options = {
-        theme = "Tomorrow",
-        --theme = "PaperColor",
-        --theme = "papercolor_dark",
-        --component_separators = { left = '', right = '' },
-        --section_separators = { left = '', right = '' },
-        section_separators = { left = '', right = '' },
-        component_separators = { left = '', right = '' },
-        --section_separators = "",
-        --component_separators = "",
-        --theme = "papercolor_dark",
-    },
-    sections = {
-        lualine_a = {
-            window, { "mode", cond = cond80 },
-        },
-        lualine_b = {
-            { "branch",      cond = cond200 },
-            { "diff",        cond = cond200 },
-            { "diagnostics", cond = cond200, },
-        },
-        lualine_c = {
-            { "filename", cond = cond40 },
-
-        },
-        lualine_x = {
-            { "copilot", },
-            --{ codeium_state,  },
-            { "encoding",   cond = cond80 },
-            { "fileformat", cond = cond200 },
-            { "filetype",   cond = cond200 },
-
-        },
-        lualine_y = {
-            { "progress", cond = cond80 },
-            { "location", cond = cond200 },
-        },
-        lualine_z = {
-            {
-                clock,
-                cond = cond40,
-            }
-
-        },
-    },
-    inactive_sections = {
-        lualine_a = { window, { "filename", cond = cond40 } }
-    },
-    tabline = {
-        -- lualine_a = {'buffers'},
-        lualine_a = {
-            {
-                "tabs",
-                mode = 2
-            },
-        },
-        -- lualine_b = {'branch'},
-        lualine_b = {},
-        lualine_c = { "filename" },
-        lualine_x = {},
-        lualine_y = {},
-        lualine_z = { "tabs" },
-    },
-    extensions = {},
+	options = {
+		theme = "Tomorrow",
+		--theme = "PaperColor",
+		--theme = "papercolor_dark",
+		--component_separators = { left = '', right = '' },
+		--section_separators = { left = '', right = '' },
+		section_separators = { left = "", right = "" },
+		component_separators = { left = "", right = "" },
+		--section_separators = "",
+		--component_separators = "",
+		--theme = "papercolor_dark",
+	},
+	sections = {
+		lualine_a = {
+			window,
+			{ "mode", cond = cond80 },
+		},
+		lualine_b = {
+			{ "branch", cond = cond200 },
+			{ "diff", cond = cond200 },
+			{ "diagnostics", cond = cond200 },
+		},
+		lualine_c = {
+			{ "filename", cond = cond40 },
+		},
+		lualine_x = {
+			{ "copilot" },
+			--{ codeium_state,  },
+			{ "encoding", cond = cond80 },
+			{ "fileformat", cond = cond200 },
+			{ "filetype", cond = cond200 },
+		},
+		lualine_y = {
+			{ "progress", cond = cond80 },
+			{ "location", cond = cond200 },
+		},
+		lualine_z = {
+			{
+				clock,
+				cond = cond40,
+			},
+		},
+	},
+	inactive_sections = {
+		lualine_a = { window, { "filename", cond = cond40 } },
+	},
+	tabline = {
+		-- lualine_a = {'buffers'},
+		lualine_a = {
+			{
+				"tabs",
+				mode = 2,
+			},
+		},
+		-- lualine_b = {'branch'},
+		lualine_b = {},
+		lualine_c = { "filename" },
+		lualine_x = {},
+		lualine_y = {},
+		lualine_z = { "tabs" },
+	},
+	extensions = {},
 })
