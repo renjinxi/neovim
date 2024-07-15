9c9ebc42c927950cfdbf630444e0d915a4f16c36
renjinxi <48796935+renjinxi@users.noreply.github.com>
2024-07-15 16:53:15 +0800

    Remove commented-out code in neotest.lua

--- lua/user/plugins/neotest.lua
+++ lua/user/plugins/neotest.lua
@@ -19,22 +19,6 @@ require("neotest").setup({
 	adapters = {
 		require("neotest-python")({
 			dap = { justMyCode = false },
-			runner = "pytest",
-			-- python = custom_python_path,
-			command = "coverage run -m pytest",
-			--env = get_python_env,
 		}),
-		--require("neotest-plenary"),
-		--require("neotest-vim-test")({
-		--ignore_file_types = { "python", "vim", "lua" },
-		--}),
 	},
-	--discovery = {
-	--root_dir = function(startpath)
-	--return require('neotest.lib').files.find_upwards(function(dir)
-	--return require('neotest.lib').files.exists(dir .. "/pyproject.toml") or
-	--require('neotest.lib').files.exists(dir .. "/.git")
-	--end, startpath) or startpath
-	--end,
-	--},
 })
