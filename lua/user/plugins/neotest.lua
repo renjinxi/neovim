f904864b0f8bbdbebdea1581726c880590771805
renjinxi <48796935+renjinxi@users.noreply.github.com>
2024-07-15 16:53:42 +0800

    Refactor neotest.lua by removing redundant functions

--- lua/user/plugins/neotest.lua
+++ lua/user/plugins/neotest.lua
@@ -1,20 +1,3 @@
-local common = require("user.config.common")
-
-local function custom_python_path(file_path)
-	local project_root = common.find_project_root_by_marker("pyproject.toml") or vim.fn.getcwd()
-	local python_interpreter = project_root .. "/.venv/bin/python" -- Adjust this based on your env path
-	return python_interpreter
-end
-
-local function get_python_env(file_path)
-	local project_root = common.find_project_root_by_marker("pyproject.toml") or vim.fn.getcwd()
-	return {
-		PYTHONPATH = project_root,
-		--PYTHONPATH = "/home/username/your_project",
-		--PYTHONPATH = "/home/username/your_project/your_module",
-	}
-end
-
 require("neotest").setup({
 	adapters = {
 		require("neotest-python")({
