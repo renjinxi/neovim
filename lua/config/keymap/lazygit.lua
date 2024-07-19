2c930665638f7793f8e3cab099cad25d0148c743
renjinxi <48796935+renjinxi@users.noreply.github.com>
2024-07-19 15:49:40 +0800

    Add lazygit keymap shortcuts for file operations

--- lua/config/keymap/lazygit.lua
+++ lua/config/keymap/lazygit.lua
@@ -1,5 +1,12 @@
 local keymap = {
 	{ "<leader>s", group = "Lazygit", nowait = false, remap = false },
 	{ "<leader>sl", ":LazyGit<cr>", desc = "Lazygit", nowait = false, remap = false },
+	{ "<leader>sa", ":Git add %<cr>", desc = "Add Current File", nowait = false, remap = false },
+	{ "<leader>sA", ":Git add .<cr>", desc = "Add All File", nowait = false, remap = false },
+	{ "<leader>sm", ":Git commit<cr>", desc = "Commit", nowait = false, remap = false },
+	{ "<leader>sd", ":Git diff %<cr>", desc = "Diff Current File", nowait = false, remap = false },
+	{ "<leader>si", ":Git diff .<cr>", desc = "Diff All File", nowait = false, remap = false },
+	{ "<leader>sD", ":Git diff --cached %<cr>", desc = "Diff Current File Cached", nowait = false, remap = false },
+	{ "<leader>sI", ":Git diff --cached .<cr>", desc = "Diff All File Cached", nowait = false, remap = false },
 }
 require("which-key").add(keymap)
