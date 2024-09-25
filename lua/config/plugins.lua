e6006bcd7b6b56f84cf8d8056060c8d7be71b666
renjinxi <48796935+renjinxi@users.noreply.github.com>
2024-09-25 09:33:48 +0800

    add leetcode plugin

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -594,4 +594,27 @@ return {
 			require("jabs").setup({})
 		end,
 	},
+
+	-- leetcode
+	{
+		"kawre/leetcode.nvim",
+		build = ":TSUpdate html",
+		dependencies = {
+			"nvim-telescope/telescope.nvim",
+			"nvim-lua/plenary.nvim", -- required by telescope
+			"MunifTanjim/nui.nvim",
+
+			-- optional
+			"nvim-treesitter/nvim-treesitter",
+			-- "rcarriga/nvim-notify",
+			"nvim-tree/nvim-web-devicons",
+		},
+		opts = {
+			cn = { -- leetcode.cn
+				enabled = true, ---@type boolean
+				translator = true, ---@type boolean
+				translate_problems = true, ---@type boolean
+			},
+		},
+	},
 }
