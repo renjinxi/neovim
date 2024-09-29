9a1ea211fba7c6587e94358473da5b329b11b4e5
renjinxi <48796935+renjinxi@users.noreply.github.com>
2024-09-29 17:09:33 +0800

    add third library path for lua

--- lua/lsp/servers/lua_ls.lua
+++ lua/lsp/servers/lua_ls.lua
@@ -1,11 +1,20 @@
 local M = {}
 
 M.settings = {
-    Lua = {
-        diagnostics = {
-            globals = { 'vim', 'bit' }
-        }
-    }
+	Lua = {
+		diagnostics = {
+			globals = { "vim", "bit" },
+		},
+		workspace = {
+			-- Add Neovim runtime path
+			library = vim.api.nvim_get_runtime_file("", true),
+			-- Make the language server aware of Neovim's built-in Lua
+			checkThirdParty = false,
+		},
+		telemetry = {
+			enable = false,
+		},
+	},
 }
 
 return M
