b57ca6c3ff003355d468fb24e3ad36adcacae87a
renjinxi <48796935+renjinxi@users.noreply.github.com>
2024-12-26 09:56:16 +0800

    add bash format

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -4,6 +4,7 @@ require("conform").setup({
 	formatters_by_ft = {
 		lua = { "stylua" },
 		python = { "ruff_format", "ruff_organize_imports", "ruff_fix" },
+		bash = { "shellharden" },
 		javascript = { "prettier" },
 		typescript = { "prettier" },
 		javascriptreact = { "prettier" },
