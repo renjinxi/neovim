342b2637820cd9c29ed049a44c48c9265440183d
renjinxi <48796935+renjinxi@users.noreply.github.com>
2025-07-09 15:37:43 +0800

    remove info

--- lua/plugins/config/overseer.lua
+++ lua/plugins/config/overseer.lua
@@ -17,10 +17,9 @@ local template_files = vim.fn.glob(template_dir .. "/*.lua", false, true)
 for _, template_file in ipairs(template_files) do
 	local ok, template_config = pcall(dofile, template_file)
 	if ok and template_config then
-		overseer.register_template(template_config)  -- 直接注册模板
-		print("Loaded template:", template_config.name)
+		overseer.register_template(template_config) -- 直接注册模板
+		-- print("Loaded template:", template_config.name)
 	else
 		print("Failed to load template:", template_file)
 	end
 end
-
--- lua/plugins/config/telescope.lua
+++ lua/plugins/config/telescope.lua
@@ -17,23 +17,6 @@ require("telescope").setup({
 		},
 	},
 	extensions = {
-		--project = {
-		--base_dirs = {
-		--"/Users/renjinxi/.pyenv/versions/3.12.0",
-		--},
-		--hidden_files = true,             -- default: false
-		--theme = "dropdown",
-		--order_by = "asc",
-		--search_by = "title",
-		--sync_with_nvim_tree = true,             -- default false
-		---- default for on_project_selected = find project files
-		--on_project_selected = function(prompt_bufnr)
-		---- Do anything you want in here. For example:
-		--project_actions.change_working_directory(prompt_bufnr, false)
-		--require("harpoon.ui").nav_file(1)
-		--end
-		--},
-
 		live_grep_args = {
 			auto_quoting = true, -- enable/disable auto-quoting
 			-- define mappings, e.g.
